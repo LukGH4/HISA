cb5fde3d4307c9fcb43f55f85d3d067a6e0724c2
Hoyeon-kang <zveilsidez@gmail.com>
2025-01-17 18:02:50 -0500

    Made Login page, Sign Up page, and 'Forgot Password?' page

---
+++ HISA/ForgotUserViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ForgotUserViewController.swift
+//  HISA
+//
+//  Created by Hoyeon Kang on 1/17/25.
+//
+
+import UIKit
+
+class ForgotUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HISA/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  HISA
+//
+//  Created by Hoyeon Kang on 1/17/25.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
