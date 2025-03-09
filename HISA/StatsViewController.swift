1b9092c7b3fb63f8693a59d1e71df6bc8714f28a
barthobillions <nhatmnguyen2000@gmail.com>
2025-03-08 21:04:53 -0500

    created table elements in stats page for parts and their failure ratios

--- HISA/StatsViewController.swift
+++ HISA/StatsViewController.swift
@@ -6,12 +6,93 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class StatsViewController: UIViewController {
+class StatsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var ref: DatabaseReference!
+    var partTypes: [String: [String: Any]] = [:]
+    var partTypeNames: [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        ref = Database.database().reference()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        fetchPartTypesAndStatuses()
 
         print("Stats Screen Loaded")
     }
+
+    func fetchPartTypesAndStatuses() {
+
+        let employeesRef = ref.child("users").child("employees")
+
+        employeesRef.observeSingleEvent(of: .value, with: { snapshot in
+            if snapshot.exists() {
+                var localPartTypes: [String: [String: Any]] = [:]
+
+                for employeeSnapshot in snapshot.children.allObjects as! [DataSnapshot] {
+                    let imagesRef = employeeSnapshot.childSnapshot(forPath: "images")
+
+                    for imageSnapshot in imagesRef.children.allObjects as! [DataSnapshot] {
+                        if let partType = imageSnapshot.childSnapshot(forPath: "part_type").value as? String,
+                           let status = imageSnapshot.childSnapshot(forPath: "status").value as? String {
+                            var partTypeEntry = localPartTypes[partType] ?? ["good": 0, "bad": 0]
+
+                            if status == "Good Part" {
+                                partTypeEntry["good"] = (partTypeEntry["good"] as? Int ?? 0) + 1
+                            } else {
+                                partTypeEntry["bad"] = (partTypeEntry["bad"] as? Int ?? 0) + 1
+                            }
+                            localPartTypes[partType] = partTypeEntry
+                        }
+                    }
+                }
+
+                DispatchQueue.main.async {
+                    self.partTypes = localPartTypes
+                    self.partTypeNames = Array(self.partTypes.keys)
+                    self.tableView.reloadData()
+                }
+            } else {
+                print("No data found")
+            }
+        }) { error in
+            print("Error retrieving data: \(error.localizedDescription)")
+        }
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return partTypeNames.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PartTypeCell", for: indexPath)
+        let partType = partTypeNames[indexPath.row]
+        cell.textLabel?.text = partType
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let partType = partTypeNames[indexPath.row]
+        if let statusCounts = partTypes[partType],
+           let goodCount = statusCounts["good"] as? Int,
+           let badCount = statusCounts["bad"] as? Int {
+            let total = goodCount + badCount
+            let failureRatio  = badCount/total * 100
+
+            let alert = UIAlertController(title: "Part Information", message: "Part Type: \(partType)\nFailure Ratio: \(failureRatio)%", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 }
+
